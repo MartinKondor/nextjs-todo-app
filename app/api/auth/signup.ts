95330a6edddb6e6a6ebd50a9942c4558c1665f87
Martin Kondor <martinkondor@gmail.com>
2024-05-02 12:09:51 +0200

    again

--- app/api/auth/signup.ts
+++ app/api/auth/signup.ts
@@ -10,7 +10,7 @@ export default async function handler(
  
     res.status(200).json({ success: true })
   } catch (error) {
-    if (error.type === 'CredentialsSignin') {
+    if (error instanceof Error) {
       res.status(401).json({ error: 'Invalid credentials.' })
     } else {
       res.status(500).json({ error: 'Something went wrong.' })
