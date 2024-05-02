db8e3a49420918a926b41e40cb7eb65437bd2a32
Martin Kondor <martinkondor@gmail.com>
2024-05-02 14:15:25 +0200

    adding data fetch functions

---
+++ app/data.ts
@@ -0,0 +1,33 @@
+import { sql } from '@vercel/postgres';
+import { TodoList, User } from './types'; 
+
+
+export async function fetchTodoLists() {
+  try {
+    const data = await sql<TodoList>`SELECT * FROM todo_list`;
+    return data.rows;
+  } catch (error) {
+    console.error('Database Error:', error);
+    throw new Error('Failed to fetch todo_list data.');
+  }
+}
+
+export async function fetchTodoList(id: string) {
+  try {
+    const data = await sql<TodoList>`SELECT * FROM todo_list WHERE id=${id}`;
+    return data.rows;
+  } catch (error) {
+    console.error('Database Error:', error);
+    throw new Error('Failed to fetch todo_list data.');
+  }
+}
+
+export async function fetchUser(id: string) {
+  try {
+    const data = await sql<User>`SELECT * FROM users WHERE id=${id}`;
+    return data.rows;
+  } catch (error) {
+    console.error('Database Error:', error);
+    throw new Error('Failed to fetch users data.');
+  }
+}
